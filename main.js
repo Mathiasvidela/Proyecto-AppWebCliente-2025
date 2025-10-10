49893187de7d5ceb7b5813f5c5a60c18eb0f2d51
mathias videla <mathias.videla@istea.com.ar>
2025-10-10 19:35:00 -0300

    Array de productos para hacerlo dinamico

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+//Formulario
+
+
 let form = document.querySelector("form");
 let modal = document.querySelector("#modal");
 let modalText = document.querySelector("#modalText");
@@ -32,17 +35,56 @@ function cerrarModal(){
     
 }
 
-let caracteristicas = document.querySelectorAll(".caracteristicas");
-let botonCerrar = document.querySelectorAll(".cerrarBoton")
 
-function mostrarCaract(){
-    caracteristicas.forEach(c => c.classList.remove("hiddenInfo"));
-    botonCerrar.forEach(b => b.classList.remove("hiddenInfo"));
+//Productos
+const ListProducts = [
+
+  // Phones
+  {name: "Zhen phone X pro", description: "Máxima potencia y diseño.", price: 156, img: "./images/phone xpro.png"},
+  {name: "Zhen phone X", description: "Rendimiento y estilo.", price: 156, img: "./images/zhenphonex.png"},
+  {name: "Zhen phone SE", description: "Tecnología esencial.", price: 156, img: "./images/zhenphoneSE.png"},
+  {name: "Zhen phone Y", description: "Confiable y accesible.", price: 156, img: "./images/zhenphone Y.png"},
+
+  // Headphones
+  {name: "ZhenHeadphones Pro Max", description: "Sonido premium.", price: 156, img: "./images/ZhenHeadphones Pro Max.png"},
+  {name: "ZhenHeadphones Elite", description: "Comodidad superior.", price: 156, img: "./images/ZhenHeadphones Elite.png"},
+  {name: "ZhenHeadphones Studio", description: "Audio profesional.", price: 156, img: "./images/ZhenHeadphones Studio.png"},
+  {name: "ZhenHeadphones Play", description: "Diversión sonora.", price: 156, img: "./images/ZhenHeadphones Play.png"},
+  {name: "ZhenHeadphones Go", description: "Ligeros y prácticos.", price: 156, img: "./images/ZhenHeadphones Go.png"},
+
+  // Watches
+  {name: "Zhen Watch Pro", description: "Elegante y potente.", price: 1750, img: "./images/zhen watch pro.png"},
+  {name: "Zhen Watch Active", description: "Deportividad y estilo.", price: 1400, img: "./images/zhen watch active.png"},
+  {name: "Zhen Watch Fit", description: "Cómodo y funcional.", price: 999, img: "./images/zhen watch fit.png"},
+  {name: "Zhen Watch Sport", description: "Ideal para entrenar.", price: 850, img: "./images/zhen watch sport.png"},
+  {name: "Zhen Watch Core", description: "Simple y versátil.", price: 850, img: "./images/zhen watch core.png"}
+
+];
+
+let productsContainer = document.querySelector("#productsContainer"); //este es el div donde van todos los productos
+
+//funcion que crea los elementos del producto
+function createProductCard(product){
+
+    //contenedor principal article
+    const article = document.createElement("article");
+    article.classList.add("products-card");
+
+    //Imagen
+    const divImg = document.createElement("div");
+    const img = document.createElement("img");
+    img.classList.add("products-card-img");
+    img.src = product.img;
+    img.alt = product.name;
+    divImg.appendChild(img);
+
+
+
+
 
 }
 
-function cerrarInfo(){
-    caracteristicas.forEach(c => c.classList.add("hiddenInfo"));
-    botonCerrar.forEach(b => b.classList.add("hiddenInfo"));
-}
-
+ListProducts.forEach(product => {
+    const newProduct = createProductCard(product);
+    productsContainer.appendChild(newProduct);
+});
